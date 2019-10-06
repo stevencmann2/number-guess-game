2f8080d0b07ea923ab3e3ef30066aad970ae9df2
Steven Mann <stevencmann2@gmail.com>
2019-10-06 15:45:02 -0500

    awaiting help, re-wrote js to skeleton process solving form

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,26 +1,38 @@
 // establuish variables //
+const correct = 0;
+const incorrect = 0;
+    
 
 // get user input using key press //
-document.onkeyup = function(event) {
+document.onkeyup = function (event) {
     //determines which key ewas pressed//
-    const userGuess = event.key
+    const userGuess = event.key;
+    console.log()
+
+        
+
+    // array for computer choices
+
+   
+    //randomly chooses a choice from the options array //
+
+    const computerDecision = [Math.floor.random() * 10 + 1];
+    
+
+
+
+
+
+    //compares the useer and computer guesses and determines the results with a conditional //
+
+    if ((userGuess === computerDecision)) {
+        //correct is a const that is needed to be made on the page//
+        correct++ 
+    } else {
+        incorrect++
+    }
+
+
+
+
 }
-
-// array for computer choices
-
-const computerChoices = [1, 2, 3, 4, 5, 6, 7, 8. 9];
-//randomly chooses a choice from the options array //
-
-const computerDecision =  computerChoices[Math.floor.random()*computerChoices.length];
-
-//compares the useer and computer guesses and determines the results with a condiiotnal //
-
-
-
-
-
-
-
-
-
-
