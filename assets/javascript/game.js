6e4d493b3cedd9c65ed35b7eabf51c70faed41df
Steven Mann <stevencmann2@gmail.com>
2019-10-06 15:11:12 -0500

    changing js to different conditionals, following video guides

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,43 +1,26 @@
+// establuish variables //
 
-// This will prompt the user to pick an integer between 1 and the limit set by maxNumber
-// It will also track the number of attempts made by the user
-const maxNumber = 10;
-const attempts = 0;
-
-
-// generates a random number between 1 and 10 (maxNumber) as determined by the computer
-const computerNumber = Math.floor(Math.random () * maxNumber + 1);
-
-//Asks the user for input REMOVE IN A MINUTE
-
-const initialQuestion = prompt("To play, pick a number between 1 and " + maxNumber);
-
-//compares the result of the computer and the user
- while (initialQuestion != computerNumber) {
-    const initialQuestion = prompt("To play, pick a number between 1 and " + maxNumber);
-    attempts += 1;
-
-    if (attempts > Maxtries) {
-        break
-        document.write("That's all Folks! It looks like you have exceeded the max amount of attempts :( Let's Try Again")
-    }
- }
-
-
-
-
-
-
-document.onkeyup = function() {
-    const userguess= 
+// get user input using key press //
+document.onkeyup = function(event) {
+    //determines which key ewas pressed//
+    const userGuess = event.key
 }
 
+// array for computer choices
+
+const computerChoices = [1, 2, 3, 4, 5, 6, 7, 8. 9];
+//randomly chooses a choice from the options array //
+
+const computerDecision =  computerChoices[Math.floor.random()*computerChoices.length];
+
+//compares the useer and computer guesses and determines the results with a condiiotnal //
 
 
-const number = 
 
 
 
 
-//This will Count the Guesses//
-// Correct Guesses//
+
+
+
+
